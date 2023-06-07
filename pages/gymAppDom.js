f02978afcb85b0de9e852cab61ebd40635d4d0f8
Katlego Mothibe <Kaymothibe3@gmail.com>
2023-06-07 15:14:38 +0200

    template for the exercise

--- pages/gymAppDom.js
+++ pages/gymAppDom.js
@@ -1,22 +1,22 @@
-// async function fetchData(equipment) {
-//     const url = 'https://exercisedb.p.rapidapi.com/exercises/equipment/' + equipment;
-//     const options = {
-//         method: 'GET',
-//         headers: {
-//             'X-RapidAPI-Key': '3606f20607msheef339fe771ec91p1cb26djsnb444be19876e',
-//             'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-//         }
-//     };
+async function fetchData(equipment) {
+    const url = 'https://exercisedb.p.rapidapi.com/exercises/equipment/' + equipment;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '3606f20607msheef339fe771ec91p1cb26djsnb444be19876e',
+            'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+        }
+    };
 
-//     try {
-//         const response = await fetch(url, options);
-//         const result = await response.text();
-//         console.log(result);
-//         return result;
-//     } catch (error) {
-//         console.error(error);
-//     }
-// }
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        console.log(result);
+        return result;
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 function onScanSuccess(decodedText, decodedResult) {
     // Handle on success condition with the decoded text or result.
