04e15dd2d9b82b776392729bda67b21d577ff0b3
progress <leswifil@gmail.com>
2023-06-07 09:42:57 +0200

    scanner added

---
+++ pages/gymAppDom.js
@@ -0,0 +1,14 @@
+
+
+
+function onScanSuccess(decodedText, decodedResult) {
+    // Handle on success condition with the decoded text or result.
+    // console.log(`Scan result: ${decodedText}`, decodedResult);
+    console.log(decodedText);
+    console.log(decodedResult);
+}
+
+var html5QrcodeScanner = new Html5QrcodeScanner(
+	"reader", { fps: 10, qrbox: 250 });
+
+html5QrcodeScanner.render(onScanSuccess);
