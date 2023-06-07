e1a227f511805a4d694280c25b1c333528e7235f
Katlego Mothibe <Kaymothibe3@gmail.com>
2023-06-07 20:37:22 +0200

    equipment

---
+++ pages/equipment.js
@@ -0,0 +1,39 @@
+async function fetchData(equipment) {
+    const url = `https://exercisedb.p.rapidapi.com/exercises/equipment/${equipment}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '3606f20607msheef339fe771ec91p1cb26djsnb444be19876e',
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+      }
+    };
+  
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      const limitedData = result.slice(0, 10); // Limit to 10 objects
+      console.log(limitedData);
+      return limitedData;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  document.addEventListener("DOMContentLoaded", async () => {
+    const equipment = localStorage.getItem('equipment');
+    document.getElementById('equip-name').innerHTML = equipment;
+  
+    try {
+      const data = await fetchData(equipment);
+  
+      console.log(data)
+  
+      let templateSource = document.querySelector('.exerciseTemplate').innerHTML;
+      let template = Handlebars.compile(templateSource);
+      let filledTemplate = template(data);
+      document.querySelector('.exercise-container').innerHTML = filledTemplate;
+    } catch (error) {
+      console.error(error);
+    }
+  });
+  
