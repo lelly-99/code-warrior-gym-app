8595bdec3de4541926e947379250a211b13fab7d
Katlego Mothibe <Kaymothibe3@gmail.com>
2023-06-08 08:07:22 +0200

    made data to be saved in localStorage after every api call

--- pages/equipment.js
+++ pages/equipment.js
@@ -9,33 +9,45 @@ async function fetchData(equipment) {
     };
   
     try {
-      const response = await fetch(url, options);
-      const result = await response.json();
-      const limitedData = result.slice(0, 10); // Limit to 10 objects
-      console.log(limitedData);
-      return limitedData;
+      // Check if data exists in local storage
+      const storedData = localStorage.getItem(equipment);
+      if (storedData) {
+        console.log('Fetching data from local storage...');
+        const parsedData = JSON.parse(storedData);
+        return parsedData;
+      } else {
+        console.log('Fetching data from API...');
+        const response = await fetch(url, options);
+        const result = await response.json();
+        const limitedData = result.slice(0, 10); // Limit to 10 objects
+  
+        // Save data to local storage
+        localStorage.setItem(equipment, JSON.stringify(limitedData));
+  
+        return limitedData;
+      }
     } catch (error) {
       console.error(error);
     }
-}
-
-document.addEventListener("DOMContentLoaded", async () => {
+  }
+  
+  document.addEventListener("DOMContentLoaded", async () => {
     const imgEl = document.getElementById('equip-img');
     const equipment = localStorage.getItem('equipment');
     document.getElementById('equip-name').innerHTML = equipment;
     imgEl.setAttribute('src', localStorage.getItem('imgSrc'));
-
+  
     try {
-        const data = await fetchData(equipment);
-
-        console.log(data)
-
-        let templateSource = document.querySelector('.exerciseTemplate').innerHTML;
-        let template = Handlebars.compile(templateSource);
-        let filledTemplate = template(data);
-        document.querySelector('.exercise-container').innerHTML = filledTemplate;
-    }catch (error) {
-        console.error(error);
+      const data = await fetchData(equipment);
+  
+      console.log(data);
+  
+      let templateSource = document.querySelector('.exerciseTemplate').innerHTML;
+      let template = Handlebars.compile(templateSource);
+      let filledTemplate = template(data);
+      document.querySelector('.exercise-container').innerHTML = filledTemplate;
+    } catch (error) {
+      console.error(error);
     }
-});
+  });
   
